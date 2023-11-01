88ea60529ec40d32ad2897db0b36bf3fefa33130
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-11-01 14:20:23 +0000

    verifications

--- verify.c
+++ verify.c
@@ -21,7 +21,7 @@ int arguments(int argc)
 {
     if(argc == 2)
     {
-        ft_printf("The program expects more than one argument to sort the stack\n")
+        ft_printf("The program expects more than one argument to sort the stack\n");
         return(1);
     }
     return(0);
@@ -29,28 +29,46 @@ int arguments(int argc)
 int check_duplicates(char **argv)
 {
     int i;
-    i = 0;
+    int j;
+
+    i = 1;
+    j = i + 1;
     while(argv[i] != NULL)
     {
-        if(argv[i + 1] == NULL)
+        if(argv[j] == NULL)
             break;
+        while(argv[j] != NULL)
+        {
+            if(ft_atoi(argv[i]) == ft_atoi(argv[j]))
+                {
+                    ft_printf("Arguments duplicated\n");
+                    return(1);
+                }
+            j++;
+        }
+        if(argv[j] == NULL)
+        {
+            i++;
+            j = i +1;
+        }
 
-        i++;
     }
+    return(0);
 }
 
 int verify(int argc, char **argv)
 {
     int i;
     i = 1;
-    if
+    if(arguments(argc) == 1)
+        return(1);
     while(argv[i] != NULL)
     {
-        if(arguments(argc) == 1)
-            return(1);
         if(is_number(argv[i]) == 1 )
-            return(1);
+            return(1);    
         i++;
     }
+    if(check_duplicates(argv) == 1)
+        return(1);
     return(0);
 }

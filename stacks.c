e52c24ddb5864530bd0407885213090819190367
dlamark- <119970138+debsalbornoz@users.noreply.github.com>
2023-11-08 13:34:54 +0000

    .

--- stacks.c
+++ stacks.c
@@ -1,39 +1,5 @@
 #include "push_swap.h"
 
-linked_list *create_linked_list()
-{
-    linked_list *l = (linked_list    *)ft_calloc(1, sizeof(linked_list));
-    l->begin = NULL;
-    l->end = NULL;
-    l->size = 0;
-    return(l);
-}
-
-node    *create_node(int value){
-
-    node    *snode = (node  *)ft_calloc(1, sizeof(snode));
-    snode->prev = NULL;
-    snode->next = NULL;
-    snode->value = value;
-    
-    return(snode);
-}
-
-void destroy_linked_list(linked_list **l_ref)
-{
-    linked_list *l = *l_ref;
-    node *p = l->begin;
-    node *aux = NULL;
-    while(p != NULL)
-    {
-        aux = p;
-        p = p->next;
-        free(aux);
-    }
-    free(l);
-    *l_ref= NULL;
-}
-
  void  add_first(linked_list *l, int value)
  {
     node    *p;
